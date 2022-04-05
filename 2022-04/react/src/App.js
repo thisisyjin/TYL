19bbabff4fc1882dfedc769c2535b684204e1cdd
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-04-05 13:38:16 +0900

    Create App.js

---
+++ 2022-04/react/src/App.js
@@ -0,0 +1,15 @@
+import Input from './component/Input'
+import InputWithStyle from './component/InputWithStyle'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Input />
+      <InputWithStyle />
+      
+    </div>
+  );
+}
+
+export default App;
