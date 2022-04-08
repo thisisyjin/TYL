b245bd06a361b94b5fc8aff279b6c02d587b5bd3
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-04-08 13:22:33 +0900

    Create compose.jsx

---
+++ 2022-04/react/src/component/compose.jsx
@@ -0,0 +1,13 @@
+import compose from 'recompose/compose';
+import withLoading from './withLoading';
+import withState from 'recompose/withState';
+const withLoadData = withState('isLoading', 'setIsLoading', false);
+
+function Component() {
+  return '완료(컴포넌트 출력)';
+}
+
+const ComponentWithLoading = withLoading('로딩 중')(Component);
+const ComponentWithLoadData = withLoadData(ComponentWithLoading); // ?
+
+compose(withLoadData, withLoading('로딩 중'))(ComponentWithLoading);
