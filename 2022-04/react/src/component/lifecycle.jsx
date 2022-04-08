24d84d3e247c877a1b357ebd0805f5ae8362742b
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-04-08 13:23:12 +0900

    Update lifecycle.jsx

--- 2022-04/react/src/component/lifecycle.jsx
+++ 2022-04/react/src/component/lifecycle.jsx
@@ -1,13 +1,33 @@
+import React from 'react';
+import lifecycle from 'recompose/lifecycle';
 import compose from 'recompose/compose';
 import withLoading from './withLoading';
-import withState from 'recompose/withState';
-const withLoadData = withState('isLoading', 'setIsLoading', false);
 
-function Component() {
-  return '완료(컴포넌트 출력)';
+function Page({ content }) {
+  return (
+    <div>
+      페이지 로딩이 완료되었습니다.
+      {content}
+    </div>
+  );
 }
 
-const ComponentWithLoading = withLoading('로딩 중')(Component);
-const ComponentWithLoadData = withLoadData(ComponentWithLoading);
+export const withLoadData = lifecycle({
+  state: { isLoading: true, content: '' },
+  componentDidMount: function () {
+    if (this.props.loadData) {
+      this.props.loadData().then((content) =>
+        this.setState({
+          isLoading: false,
+          content,
+        })
+      );
+    }
+  },
+});
 
-compose(withLoadData, withLoading('로딩 중'))(ComponentWithLoading);
+export const PageWithLoadData = withLoadData(Page);
+export const PageWithLoadDataAndLoading = compose(
+  withLoadData,
+  withLoading('서버 요청중')
+)(Page);
