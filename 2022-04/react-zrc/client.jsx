d8327ca0f97afec920ce0d3f536b6a09e933d20c
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-04-10 14:26:53 +0900

    Create client.jsx

---
+++ 2022-04/react-zrc/client.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import RSP from './RSP';
+
+ReactDom.render(<RSP />, document.querySelector('#root'));
+// render jsx file at #root (index.html)
