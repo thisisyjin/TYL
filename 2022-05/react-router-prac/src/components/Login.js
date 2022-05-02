74ad9c8305bbdb1ef95c54ad97d783a67b81c122
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-05-02 14:20:46 +0900

    pages

---
+++ 2022-05/react-router-prac/src/components/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+    return (
+        <div>
+            <input placeholder="ID" />
+            <input type="password" placeholder="PW" />
+            <button type="submit">로그인</button>
+        </div>
+    );
+}
+
+export default Login;
