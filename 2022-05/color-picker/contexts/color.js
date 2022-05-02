0e0af07d24d72b28b61f45065cced7bf3df1884f
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-05-02 16:36:00 +0900

    context

---
+++ 2022-05/color-picker/contexts/color.js
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react";
+
+const ColorContext = createContext({
+    state: { color: 'black', subcolor: 'red' },
+    actions: {
+        setColor: () => { },
+        setSubColor: () => { },
+    }
+});
+
+const ColorProvider = ({ children }) => {
+    const [color, setColor] = useState('black');
+    const [subcolor, setSubColor] = useState('red');
+
+    const value = {
+        state: { color, subcolor },
+        actions: { setColor, setSubColor }
+    };
+    return (
+        <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+    );
+};
+
+// ColorContext.Provider = ColorProvider (value, children 포함
+// ColorContext.Consumer = ColorConsumer 
+const { Consumer: ColorConsumer } = ColorContext;
+export { ColorProvider, ColorConsumer };
+
+export default ColorContext;
