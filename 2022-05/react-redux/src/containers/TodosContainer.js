962d6d7d59777e51cf1fdfa0fa15401dfb3fef5d
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-05-04 18:58:30 +0900

    container

---
+++ 2022-05/react-redux/src/containers/TodosContainer.js
@@ -0,0 +1,31 @@
+import { connect } from "react-redux";
+import { changeInput, insert, toggle, remove } from "../modules/todos";
+import Todos from "../components/Todos";
+
+const TodoContainer = ({ input, todos, changeInput, insert, toggle, remove }) => {
+
+    return (
+        <Todos
+            input={input}
+            todos={todos}
+            onChangeInput={changeInput}
+            onInsert={insert}
+            onToggle={toggle}
+            onRemove={remove}
+        />
+    );
+};
+
+export default connect(
+    // state.todos 가져옴
+    ({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos,
+    }),
+    {
+        changeInput,
+        insert,
+        toggle,
+        remove
+    },
+)(TodoContainer);
