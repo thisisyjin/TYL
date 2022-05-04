9461a07bf4ba87679a176c3408a933c68b120424
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-05-04 19:00:45 +0900

    src

---
+++ 2022-05/react-redux/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import App from './App';
+import './index.css'
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
