95cf3a4b383381515d998ae6d4e1a713d11abb36
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-05-19 18:11:09 +0900

    pg

---
+++ 2022-05/blog-frontend/src/pages/PostListPage.js
@@ -0,0 +1,12 @@
+import HeaderContainer from '../containers/common/HeaderContainer';
+
+const PostListPage = () => {
+  return (
+    <div>
+      <HeaderContainer />
+      <div>this is contents!</div>
+    </div>
+  );
+};
+
+export default PostListPage;
