dc0e3905f7eadb58ba0e954d5ce75dee426e74bb
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-08-08 14:22:13 +0900

    header

---
+++ 2022-08/starbucks/js/main.js
@@ -0,0 +1,38 @@
+const searchEl = document.querySelector('.search');
+// const searchInputEl = document.querySelector('.search input');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색'); // HTML 속성을 설정
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
